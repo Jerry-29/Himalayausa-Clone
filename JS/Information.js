4a6099f80ec3e0130a2219d8cb35f12342a37c61
Govindsingh29 <govindsingh0498@gmail.com>
2021-11-11 18:49:25 +0530

    paymentPage

--- JS/Information.js
+++ JS/Information.js
@@ -1,6 +1,6 @@
 
-var InfoStore=JSON.parse(localStorage.getItem("userHimalaya")||[])
-console.log(InfoStore);
+//var InfoStore=JSON.parse(localStorage.getItem("userHimalaya")||[])
+//console.log(InfoStore);
 // let userLoginData={
 //     uname:uname,
 //     ulastName:ulastName,
@@ -13,30 +13,25 @@ console.log(InfoStore);
 //     uZip:uZip,
 //     uPhon:uPhon,
 //  } 
-var continueToShip=document.querySelector(".continueToShip") 
-window.onload=function(){
-    var form=document.getElementById("form")
+// window.onload=function(){
+//     var form=document.getElementById("form")
 
-var emailInp=document.querySelector(".emailInp")
-emailInp.value=InfoStore[InfoStore.length-1].uMail
+// var emailInp=document.querySelector(".emailInp")
+// emailInp.value=InfoStore[InfoStore.length-1].uMail
 
-var nameInp=document.querySelector(".nameInp")
-nameInp.value=InfoStore[InfoStore.length-1].uname
+// var nameInp=document.querySelector(".nameInp")
+// nameInp.value=InfoStore[InfoStore.length-1].uname
 
-var lastInp=document.querySelector(".lastInp")
-lastInp.value=InfoStore[InfoStore.length-1].ulastName
+// var lastInp=document.querySelector(".lastInp")
+// lastInp.value=InfoStore[InfoStore.length-1].ulastName
 
-}
+// }
 
-function submitInfo(event){
-event.preventDefault()
-if(localStorage.getItem("UserAddress")===null){
-    localStorage.setItem("UserAddress",JSON.stringify([]))
-}
-
- var address=document.querySelector(".address").value
- var appartment=document.querySelector(".appartment").value
- var city=document.querySelector(".city").value
+var address=document.getElementsByClassName(".address")
+var k=Array.from(address)[1]
+console.log(k.value);
+var appartment=document.querySelector(".appartment").value
+var city=document.querySelector(".city").value
 var Country=document.querySelector(".Country").value
 var State=document.querySelector(".State").value
 var Zip=document.querySelector(".Zip").value
@@ -46,22 +41,43 @@ var continueToShip=document.querySelector(".continueToShip")
 
 var addressArr=JSON.parse(localStorage.getItem("UserAddress"))
 
-let userAddress={
-    uAddress:address,
-    uapparment:appartment,
-    uCity:city,
-    uCountry: Country,    
-    uState:State,
-    uZip:Zip,
-    uPhon:phon,
- } 
- continueToShip.onclick=function(){
-addressArr.push(userAddress)
-localStorage.setItem("UserAddress",JSON.stringify(addressArr))
-}
+function submitInfo(event){
+event.preventDefault()
+// let userAddress={
+//     uAddress:address,
+//     uapparment:appartment,
+//     uCity:city,
+//     uCountry: Country,    
+//     uState:State,
+//     uZip:Zip,
+//     uPhon:phon,
+//  } 
+//  continueToShip.onclick=function(){
+    // if(address==""||appartment==""||city==""||State==""||Zip==""||phon==""){
+        // alert("Field can not empty")
+        console.log(address);
+        console.log(appartment);
+        console.log(city);
+        console.log(State);
+        console.log(Zip);
+        console.log(phon);
+    // }
+    // // else if(phon.length!=10){
+    // //     alert("Invalid Phone Number")
+    // // }
+    // else{
+    //     addressArr.push(userAddress)
+    //    localStorage.setItem("UserAddress",JSON.stringify(addressArr))
+    //    window.location.href="../HTML/Shipping.html"
+    // }
+
+
 
 }
 
+if(localStorage.getItem("UserAddress")===null){
+    localStorage.setItem("UserAddress",JSON.stringify([]))
+}
 // prodcuts
 var prodArr=JSON.parse(localStorage.getItem("HimalayaUsStorage"))||[]
 var parent=document.querySelector(".Imagenameprice")
@@ -80,7 +96,7 @@ function showProducts(){
         Pimg.setAttribute("class","Pimg")
         
         Pimg.src=element.Image
-        price.innerHTML="$ "+element.Price+" x "+element.quant
+        price.innerHTML="$"+element.Price+" x "+element.quant
         name.innerHTML=element.Name
         // pQt.innerHTML=element.quant
         pdiv.append(Pimg,name)
@@ -96,15 +112,21 @@ var totalSum=document.querySelector(".totalSum")
 for(var i=0;i<prodArr.length;i++){
     sum+=Number(prodArr[i].Price)*Number(prodArr[i].quant)
 }
-subtotal.innerHTML="$ "+sum.toFixed(2)
-totalSum.innerHTML="USD $"+sum.toFixed(2)
+subtotal.innerHTML="$"+sum.toFixed(2)
+totalSum.innerHTML="$"+sum.toFixed(2)
 
 var coupn=document.getElementById("coupn")
 var coupnBt=document.querySelector(".coupnBt")
 coupnBt.onclick=function(){
 if(coupn.value==="FIRSTUSER50"){
-    totalSum.innerHTML="USD $"+((sum*50)/100).toFixed(2)
+    totalSum.innerHTML="$"+((sum*50)/100).toFixed(2)
 }else{
     alert("Invalid Coupon Code .Try FIRSTUSER50")
 }
 }
+
+var goToCart=document.querySelector(".goToCart")
+goToCart.onclick=function(){
+    window.location.href="../HTML/Cart.html"
+}
+
